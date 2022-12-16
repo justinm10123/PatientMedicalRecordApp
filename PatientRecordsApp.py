143bdd047482940a5da6a33d40957fe18bb27228
Justin Meyer <justinm.10123@hotmail.com>
2022-12-15 23:02:51 -0500

    testing the ability to call lines from the txt files

--- PatientRecordsApp.py
+++ PatientRecordsApp.py
@@ -1,3 +1,21 @@
 #Georgetown Pediatrics Patient Medical Records Application
+import os
 import tkinter as tk
 
+
+#file directory for all patient record txt files
+text_file_directory = "G:/Georgetown Data/Discrete"
+
+readfile_Person = open("G:/Georgetown Data/Discrete/Person.txt","r")
+
+readlinestest = readfile_Person.readlines()
+
+for line in readlinestest:
+    # split the line into words
+    words = line.split("|")
+    first_word = words[0:100]
+
+
+
+
+print(first_word)
