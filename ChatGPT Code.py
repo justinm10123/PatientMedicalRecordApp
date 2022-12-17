c6e60e49d8a59c8e99442f9dae54013d2068fd40
Justin Meyer <justinm.10123@hotmail.com>
2022-12-16 22:56:06 -0500

    formatting.

--- ChatGPT Code.py
+++ ChatGPT Code.py
@@ -6,33 +6,26 @@ with open("G:/Georgetown Data/Discrete/Person.txt", 'r') as file:
   # Print the headers
   #print(headers)
 
-  # Ask the user for an input
-  search_term = input("Enter a Patient ID #, First and Last name, or type Quit to Stop: ")
-  split_search_term = search_term.split()
-
   # Read the rest of the lines in the file
   for line in file:
     # Split the line into a list of words
     words = line.split("|")
+
+    #asking for user input
+    search_term = input("Enter a Patient ID #, First and Last name, or type Quit to Stop: ")
+    split_search_term = search_term.split()
+
+
     # Check if the first word in the line matches the search term
-    if words[0] == search_term:
+    if words[0].lower() == search_term.lower() or (words[2].lower() == split_search_term[0].lower() and words[5].lower() == split_search_term[1].lower()):
       # If it does, print the entire line
       print(line)
 
-      if search_term.lower() == "quit":
-        break
-      else:
-        search_term = input("Enter another search term or Quit to Stop: ")
+    #if user input is quit is breaks the loop
+    if search_term.lower() == "quit":
+      break
 
-    elif words[2] == split_search_term[0] and words[5] == split_search_term[1]:
-      print(line)
-
-      if search_term.lower() == "quit":
-        break
-      
-      else:
-        search_term = input("Enter another search term or Quit to Stop: ")
-
+    ############## need to figure out why it does not print line when first and last name are entered ###################
         
 
   
