2ae141b578add57c769a9208de05f1992ac1d17a
Justin Meyer <justinm.10123@hotmail.com>
2022-12-18 15:58:07 -0500

    Adding all file path directories, will need to change initial directory location once all code and files are located on final hand off hard drive.

--- PatientRecordsApp.py
+++ PatientRecordsApp.py
@@ -5,7 +5,32 @@ import tkinter as tk
 
 #file directory for all patient record txt files
 text_file_directory = "G:/Georgetown Data/Discrete"
+
 person_file = open("G:/Georgetown Data/Discrete/Person.txt","r")
+patient_file = open("G:/Georgetown Data/Discrete/Patient.txt","r")
+address_file = open("G:/Georgetown Data/Discrete/Address.txt","r")
+allergy_file = open("G:/Georgetown Data/Discrete/Allergy.txt","r")
+br_account_file = open("G:/Georgetown Data/Discrete/br_Account.txt","r")
+clinical_vital_file = open("G:/Georgetown Data/Discrete/ClinicalVital.txt","r")
+flag_file = open("G:/Georgetown Data/Discrete/Flag.txt","r")
+ins_coverage_patient_file = open("G:/Georgetown Data/Discrete/InsCoveragePatient.txt","r")
+insurance_coverage_file = open("G:/Georgetown Data/Discrete/InsuranceCoverage.txt","r")
+insurance_plan_file = open("G:/Georgetown Data/Discrete/InsurancePlan.txt","r")
+medications_file = open("G:/Georgetown Data/Discrete/Medications.txt","r")
+migration_ins_export_file = open("G:/Georgetown Data/Discrete/MigrationInsExport.txt","r")
+migration_proc_export_file = open("G:/Georgetown Data/Discrete/MigrationProcExport.txt","r")
+migration_referring_export_file = open("G:/Georgetown Data/Discrete/MigrationReferringExport.txt","r")
+migration_schedule_export_file = open("G:/Georgetown Data/Discrete/MigrationScheduleExport.txt","r")
+next_of_kin_file = open("G:/Georgetown Data/Discrete/NextOfKin.txt","r")
+orders_detail_tracking_file = open("G:/Georgetown Data/Discrete/OrdersDetailTracking.txt","r")
+pat_hist_family_file = open("G:/Georgetown Data/Discrete/PatHistFamily.txt","r")
+pat_hist_medical_file = open("G:/Georgetown Data/Discrete/PatHistMedical.txt","r")
+pat_hist_social_file = open("G:/Georgetown Data/Discrete/PatHistSocial.txt","r")
+pat_hist_surgical_file = open("G:/Georgetown Data/Discrete/PatHistSurgical.txt","r")
+patient_flags_file = open("G:/Georgetown Data/Discrete/PatientFlags.txt","r")
+patient_general_notes_file = open("G:/Georgetown Data/Discrete/PatientGeneralNotes.txt","r")
+problem_list_file = open("G:/Georgetown Data/Discrete/ProblemList.txt","r")
+vac_pat_file = open("G:/Georgetown Data/Discrete/VacPat.txt","r")
 
 search_term = input("Enter a Patient ID #, First and Last name, or type Quit to Stop: ")
 split_search_term = search_term.split()
